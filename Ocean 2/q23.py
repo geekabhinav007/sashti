903e4489ec9143c758d07a335cc3ab68482f0858
geekabhinav007 <abhinav26january@gmail.com>
2023-10-01 23:48:55 +0530

    23 completed

---
+++ Ocean 2/q23.py
@@ -0,0 +1,6 @@
+p = int(input("Enter Principal Amount : "))
+r = float(input("Enter Rate of intrest per annum  : "))
+t = int(input("Enter Time priod in year "))
+
+si = (p*r*t)/100
+print("Simple intrest is ", si)
