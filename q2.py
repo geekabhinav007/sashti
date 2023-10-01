4a73bebce5220f11fe04b487a69da38b9473dcc1
geekabhinav007 <abhinav26january@gmail.com>
2023-10-01 15:56:13 +0530

    q2 completed

--- q2.py
+++ q2.py
@@ -1,3 +1,9 @@
 # 2. Take as input a number n
-#  and print the square, cube and 2n
+#  and print the square, cube and 2 to the power n
 #  of the number (1 point).
+
+n = int(input("Enter the number n : "))
+
+print("Square  of ", n, " is ", n**2)
+print("Cube  of ", n, " is ", n**3)
+print("TWo to the power of ", n, " is ", 2**n)
