eed607250f1a16a62c18b4dfdc1cb49d8b6bdc77
geekabhinav007 <abhinav26january@gmail.com>
2023-10-01 16:40:48 +0530

    3rd completed

--- q3.py
+++ q3.py
@@ -4,3 +4,10 @@
 # **
 # ***
 # ****
+
+n = int(input("Enter number of row of traiangle you want to print : "))
+
+for i in range(1, n+1):
+    for j in range(0, i):
+        print("*", end=" ")
+    print()
